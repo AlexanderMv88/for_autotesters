c1a86419539d5a92422918343fe8a265b80ed31c
AlexanderMv88 <postalforever1988@gmail.com>
2022-04-28 18:47:19 +0300

    add jest example

---
+++ test/b2b.warm-on.test.js
@@ -0,0 +1,62 @@
+const { By, Key, Builder } = require("selenium-webdriver");
+require("chromedriver");
+const { until } = require("selenium-webdriver");
+
+let driver;
+
+//Группирует тесты
+describe('Тесты функционала https://b2b.warm-on.ru/', () => {
+    //Перед всеми тестами выполняется 
+    beforeAll(async() => {
+        //--Создаю и настраиваю WebDriver
+        /*let driver Создаем переменную driver
+        new Builder() Начинаем создавать экземпляр WebDriver. await означает дождаться выполнения асинхронной функции
+        .forBrowser("chrome") Настраивает целевой браузер для клиентов, созданных этим экземпляром
+        .build() Создает клиент WebDriver на основе конфигурации*/
+        driver = await new Builder().forBrowser("chrome").build();
+        //Разворачиваю браузер в полноэкранный режим
+        await driver.manage().window().maximize()
+    }, 30000);
+
+    //После всех тестов выполняется
+    afterAll(async() => {
+        await driver.quit();
+    }, 40000);
+
+    //Тест
+    test('Должен придти к заголовку "Регистрация"', async() => {
+        //Перехожу на страницу:
+        await driver.get("https://b2b.warm-on.ru/");
+        //Жду 2 секунд чтобы загрузилась страница
+        await driver.sleep(2000)
+            //Скролю до кнопки и нажимаю - "Стать партнером"
+        await scrollAndClickOnWebComponent(driver, 1300, "Стать партнером");
+        //Сохраняю заглавие страницы в переменную title
+        var title = await driver.getTitle();
+        //Проверяю, что значение переменной title соответствует "Регистрация"
+        expect(title).toEqual("Регистрация");
+    });
+
+    //Тут можно писать еще тесты
+});
+
+
+
+
+async function scrollAndClickOnWebComponent(driver, scroll, text) {
+
+
+    await driver.executeScript("window.scrollTo(0, " + scroll + ")");
+    await driver.sleep(300);
+
+    const element = driver.wait(
+        //Ищет пока элемент не будет найден или
+        until.elementLocated(By.xpath("//*[contains(text(), '" + text + "')]")),
+        // не пройдет 5 секунд
+        5000
+    );
+
+    //--Выполняю клик по WebElement-у
+    const actions = driver.actions({ async: true });
+    await actions.click(element).perform();
+}
